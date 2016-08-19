aa8e5b3937a890745c287844cafabc5025331435
James Johnson <jameswj111@gmail.com>
2016-08-19 13:09:28 -0400

    add es5 syntax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,17 +16,17 @@
 
 function renderStrikes(strike){
 
-  let $card       = $('<ul>')
-  let $location   = $('<li>')
-  let $names      = $('<li>').text('People killed: Unknown')
-  let $summary    = $('<li>').text(strike.narrative)
-  let $deaths     = $('<li>').text(strike.deaths + ' Deaths')
-  let $civilians  = $('<li>').text(strike.civilians + ' civilians killed')
-  let $link       = $('<p>').append($('<a target="_BLANK">')
+  var $card       = $('<ul>')
+  var $location   = $('<li>')
+  var $names      = $('<li>').text('People killed: Unknown')
+  var $summary    = $('<li>').text(strike.narrative)
+  var $deaths     = $('<li>').text(strike.deaths + ' Deaths')
+  var $civilians  = $('<li>').text(strike.civilians + ' civilians killed')
+  var $link       = $('<p>').append($('<a target="_BLANK">')
                             .attr('href', strike.bij_link)
                             .text(strike.bij_link))
-  let $date       = strike.date.replace('T00:00:00.000Z', '')
-  let $saveButton = $('<button>').addClass('save').text('SAVE')
+  var $date       = strike.date.replace('T00:00:00.000Z', '')
+  var $saveButton = $('<button>').addClass('save').text('SAVE')
 
   // conditionals for rendering below
   // check to see if there's a town included in location data
@@ -60,10 +60,10 @@ function renderStrikes(strike){
 
 
 function getData(){
-  $.get('/strikes').done((data)=> {
+  $.get('/strikes').done(function(data){
     // filter through data for search
-    filtered = data.filter((strike)=> {
-      let $h3 = $('<h3>')
+    var filtered = data.filter(function(strike){
+      var $h3 = $('<h3>')
 
       if ($('#dropdown').val()){
         $('.resultHeader').text('Search Results')
@@ -78,7 +78,7 @@ function getData(){
     if(filtered.length === 0){
       $('#results').append('<h4>').text('NO RESULTS')
     } else {
-      filtered.forEach((filtered)=> {
+      filtered.forEach(function(filtered){
         renderStrikes(filtered)
       })
     }
@@ -87,9 +87,9 @@ function getData(){
 
 
 function addStrike(){
-  let $siblings = $(this).siblings()
+  var $siblings = $(this).siblings()
 
-  let data = {
+  var data = {
     date: $siblings.eq(0).text(),
     location: $siblings.eq(1).text(),
     narrative: $siblings.eq(2).text(),
@@ -99,7 +99,7 @@ function addStrike(){
     report: $siblings.eq(6).text()
   }
 
-  $.post('/save', data).done((response)=> {
+  $.post('/save', data).done(function(response){
   })
 }
 
@@ -108,19 +108,19 @@ function getSavedStrikes(){
   $('#saved').empty()
              .append($('<h4>').text('Saved Reports'))
 
-  $.get('/save').done((data)=> {
+  $.get('/save').done(function(data){
 
-    data.forEach((strike)=> {
-      let $date      = $('<h3>').append(strike.date).val(strike.id)
-      let $location  = $('<li>').text(strike.location)
-      let $narrative = $('<li>').text(strike.narrative)
-      let $deaths    = $('<li>').text(strike.deaths)
-      let $civilians = $('<li>').text(strike.civilians)
-      let $names     = $('<li>').text(strike.names)
-      let $report    = $('<p>').append($('<a target="_BLANK">')
+    data.forEach(function(strike){
+      var $date      = $('<h3>').append(strike.date).val(strike.id)
+      var $location  = $('<li>').text(strike.location)
+      var $narrative = $('<li>').text(strike.narrative)
+      var $deaths    = $('<li>').text(strike.deaths)
+      var $civilians = $('<li>').text(strike.civilians)
+      var $names     = $('<li>').text(strike.names)
+      var $report    = $('<p>').append($('<a target="_BLANK">')
                                 .attr('href', strike.report)
                                 .text(strike.report))
-      let $delete    = $('<button>').addClass('delete').text('DELETE')
+      var $delete    = $('<button>').addClass('delete').text('DELETE')
 
       // check to see if civilians were killed
       if (strike.civilians === '' || strike.civilians === '0'){
@@ -129,7 +129,7 @@ function getSavedStrikes(){
         $civilians.css('color', 'red')
       }
 
-      let $oneResult = $('<ul>').append($date, $location, $narrative, $deaths, $civilians, $names, $report, $delete)
+      var $oneResult = $('<ul>').append($date, $location, $narrative, $deaths, $civilians, $names, $report, $delete)
 
       $('#saved').append($oneResult)
 
@@ -140,32 +140,29 @@ function getSavedStrikes(){
 
 
 function deleteStrike(){
-  let $currentList = $(this).parent()
-  let id           = Number.parseInt($(this).siblings().eq(0).val())
+  var $currentList = $(this).parent()
+  var id           = Number.parseInt($(this).siblings().eq(0).val())
 
   $.ajax({
     url: '/save/' + id,
     method: 'delete'
-  }).done(()=> {
+  }).done(function(){
     $currentList.empty()
   })
 }
 
 
+$(function(){
+  $('#dropdown').change(function(){
+    $('#results').empty()
+                 .append($('<h4>')
+                 .addClass('resultHeader'))
 
-// $(document).on('turbolinks:load', function() {
-  // clear search results for each click, get data
-  $(()=> {
-    $('#dropdown').change(()=> {
-      $('#results').empty()
-                   .append($('<h4>')
-                   .addClass('resultHeader'))
-
-      getData()
-    })
-
-    $('#savedSearches').click(()=> {
-      getSavedStrikes()
-    })
+    getData()
   })
-// });
+
+  $('#savedSearches').click(function(){
+    getSavedStrikes()
+  })
+})
+
